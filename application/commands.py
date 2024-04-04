12102c579d7bc9cd06df95c3104fd1f25100de2e
Adam Shimali <adam.shimali@gmail.com>
2024-04-04 13:30:19 +0100

    command for loading local db from heroku db

--- application/commands.py
+++ application/commands.py
@@ -150,3 +150,76 @@ def set_fields(consideration, row):
         consideration.legislation = {"link_text": legistlation, "link_url": None}
 
     return consideration
+
+
+@consider_cli.command("load-data")
+def load_data():
+    import subprocess
+    import sys
+    import tempfile
+
+    from flask import current_app
+
+    # check heroku cli installed
+    result = subprocess.run(["which", "heroku"], capture_output=True, text=True)
+
+    if result.returncode == 1:
+        print("Heroku CLI is not installed. Please install it and try again.")
+        sys.exit(1)
+
+    # check heroku login
+    result = subprocess.run(["heroku", "whoami"], capture_output=True, text=True)
+
+    if "Error: not logged in" in result.stderr:
+        print("Please login to heroku using 'heroku login' and try again.")
+        sys.exit(1)
+
+    print("Starting load data into", current_app.config["SQLALCHEMY_DATABASE_URI"])
+    if (
+        input(
+            "Completing process will overwrite your localdatabase. Enter 'y' to continue, or anything else to exit. "
+        )
+        != "y"
+    ):
+        print("Exiting without making any changes")
+        sys.exit(0)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = os.path.join(tempdir, "latest.dump")
+
+        # get the latest dump from heroku
+        result = subprocess.run(
+            [
+                "heroku",
+                "pg:backups:download",
+                "-a",
+                "dluhc-planning-considerations",
+                "-o",
+                path,
+            ]
+        )
+
+        if result.returncode != 0:
+            print("Error downloading the backup")
+            sys.exit(1)
+
+        # restore the dump to the local database
+        subprocess.run(
+            [
+                "pg_restore",
+                "--verbose",
+                "--clean",
+                "--no-acl",
+                "--no-owner",
+                "-h",
+                "localhost",
+                "-d",
+                "dluhc-planning-considerations",
+                path,
+            ]
+        )
+        print(
+            "\n\nRestored the dump to the local database using pg_restore. You can ignore warnings from pg_restore."
+        )
+
+    print("Data loaded successfully")
