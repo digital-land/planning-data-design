a987ed3db6c3f4df1f30f58a5454a0f55c06e9a5
colmjude <colmjude@gmail.com>
2024-04-22 13:33:53 +0300

    Add specific forms for structured data questions

--- application/blueprints/questions/forms.py
+++ application/blueprints/questions/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, StringField, TextAreaField
+from wtforms import RadioField, StringField, TextAreaField, URLField
 
 
 class TextareaForm(FlaskForm):
@@ -33,3 +33,31 @@ class SingleChoiceFormOther(FlaskForm):
     def __init__(self, label="Title", *args, **kwargs):
         super(SingleChoiceFormOther, self).__init__(*args, **kwargs)
         self.choice.label.text = label
+
+
+class LifecycleStagesForm(FlaskForm):
+    name = StringField("Stage name")
+    description = TextAreaField("Description")
+    actors = StringField("Who is involved?")
+    output = TextAreaField("What is produced?")
+    impact = TextAreaField("Impact")
+
+
+class ExistingDataForm(FlaskForm):
+    publisher = StringField("Publisher", description="Who publishes the data?")
+    url = URLField("URL", "A url for the data")
+    fields = StringField(
+        "Fields",
+        description="List the fields included in the data. Separate the fields with a ;",
+    )
+    licence = StringField(
+        "Licence", description="Include the licence the data has been published under."
+    )
+    attribution = TextAreaField(
+        "Attribution",
+        description="If attribution is needed to use the data include it here.",
+    )
+    coverage = StringField(
+        "Coverage",
+        description="Whats the coverage of the dataset? For example, national, partial, local authority area, etc",
+    )
