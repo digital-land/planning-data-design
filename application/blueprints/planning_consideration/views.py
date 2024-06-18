a11ea297f2059361f8773e544edc5f9c659514a8
Adam Shimali <adam.shimali@gmail.com>
2024-06-18 15:57:11 +0100

    bugfix for stage timeline

--- application/blueprints/planning_consideration/views.py
+++ application/blueprints/planning_consideration/views.py
@@ -190,11 +190,7 @@ def _create_or_update_consideration(form, attributes, is_new=False, consideratio
 def _extract_changes_of_type(consideration, attr_name):
     if consideration.changes is None:
         return None
-    return [
-        change
-        for change in consideration.changes
-        if attr_name in change["changes"].keys()
-    ]
+    return [change for change in consideration.changes if attr_name == change["field"]]
 
 
 @planning_consideration.route("/")
