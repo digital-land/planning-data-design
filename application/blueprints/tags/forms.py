642ac30956ec45ccc13f7ef6bca690b74c947768
Adam Shimali <adam.shimali@gmail.com>
2024-12-10 15:04:00 +0000

    Add/edit/remove tags

--- application/blueprints/tags/forms.py
+++ application/blueprints/tags/forms.py
@@ -1,6 +1,9 @@
+from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, ValidationError
+from wtforms import SelectField, StringField, ValidationError
 from wtforms.validators import DataRequired
+
+from application.models import Tag
 
 
 class TagForm(FlaskForm):
@@ -15,11 +18,16 @@ class TagForm(FlaskForm):
 
     def validate_name(self, field):
         self.sanitize_tag_name(field)
-        # Check if tag with this name already exists
-        from application.models import Tag
-
         existing_tag = Tag.query.filter(Tag.name == field.data).first()
         if existing_tag:
-            raise ValidationError("A tag with this name already exists")
+            # If we're editing an existing tag (URL has tag_id) and the name hasn't changed,
+            # then this is valid - it's the same tag
+            tag_id = request.view_args.get("tag_id")
+            if not tag_id or str(existing_tag.id) != tag_id:
+                raise ValidationError("A tag with this name already exists")
 
     name = StringField("Name", validators=[DataRequired()])
+
+
+class AddTagForm(FlaskForm):
+    tags = SelectField("Name", validators=[DataRequired()])
--- application/blueprints/tags/views.py
+++ application/blueprints/tags/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, abort, redirect, render_template, url_for
 
-from application.blueprints.tags.forms import TagForm
+from application.blueprints.tags.forms import AddTagForm, TagForm
 from application.extensions import db
-from application.models import Tag
+from application.models import Consideration, Tag
 
 tags = Blueprint(
     "tags",
@@ -21,26 +21,75 @@ def index():
 def add():
     form = TagForm()
     action_url = url_for("tags.add")
+    action_text = "Add tag"
     if form.validate_on_submit():
         tag = Tag(name=form.name.data)
         db.session.add(tag)
         db.session.commit()
         return redirect(url_for("tags.index"))
-    return render_template("tags/tag-form.html", form=form, action_url=action_url)
+    return render_template(
+        "tags/tag-form.html", form=form, action_url=action_url, action_text=action_text
+    )
 
 
-@tags.route("/<string:tag_id>", methods=["GET", "POST"])
-def tag(tag_id):
+@tags.route("/<string:tag_id>/edit", methods=["GET", "POST"])
+def edit_tag(tag_id):
     tag = Tag.query.get(tag_id)
     if tag is None:
         abort(404)
     form = TagForm(obj=tag)
-    action_url = url_for("tags.tag", tag_id=tag_id)
+    action_url = url_for("tags.edit_tag", tag_id=tag_id)
+    action_text = "Edit tag"
     if form.validate_on_submit():
         tag.name = form.name.data
         db.session.add(tag)
         db.session.commit()
         return redirect(url_for("tags.index"))
     return render_template(
-        "tags/tag-form.html", tag=tag, form=form, action_url=action_url
+        "tags/tag-form.html",
+        tag=tag,
+        form=form,
+        action_url=action_url,
+        action_text=action_text,
+    )
+
+
+@tags.route("/<string:consideration_id>/add", methods=["GET", "POST"])
+def tag_consideration(consideration_id):
+    consideration = Consideration.query.get(consideration_id)
+    if consideration is None:
+        abort(404)
+
+    form = AddTagForm()
+    form.tags.choices = [
+        (tag.id, tag.name) for tag in Tag.query.order_by(Tag.name).all()
+    ]
+
+    if form.validate_on_submit():
+        tag = Tag.query.get(form.tags.data)
+        if tag not in consideration.tags:
+            consideration.tags.append(tag)
+            db.session.add(consideration)
+            db.session.commit()
+        return redirect(
+            url_for("planning_consideration.consideration", slug=consideration.slug)
+        )
+    return render_template(
+        "tags/tag-consideration-form.html",
+        form=form,
+        consideration=consideration,
+    )
+
+
+@tags.get("/<string:consideration_id>/<string:tag_id>/remove")
+def remove_tag(consideration_id, tag_id):
+    consideration = Consideration.query.get(consideration_id)
+    tag = Tag.query.get(tag_id)
+    if consideration is None or tag is None:
+        abort(404)
+    consideration.tags.remove(tag)
+    db.session.add(consideration)
+    db.session.commit()
+    return redirect(
+        url_for("planning_consideration.consideration", slug=consideration.slug)
     )
