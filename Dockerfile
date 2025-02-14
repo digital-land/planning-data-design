57e1899d99ab1cce11ba69b76837de5ab44c3223
Adam Shimali <adam.shimali@gmail.com>
2025-02-14 17:03:08 +0000

    move config up out of package

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# syntax=docker/dockerfile:1
+FROM python:3.10-slim
+WORKDIR /code
+
+ENV FLASK_CONFIG=application.config.DevelopmentConfig
+ENV FLASK_APP=application.wsgi:app
+
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_RUN_PORT=5050
+ENV FLASK_DEBUG=1
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    gcc g++ git libproj-dev proj-bin gdal-bin wget gnupg2  \
+    && rm -rf /var/lib/apt/lists/*
+
+
+RUN echo "deb http://apt.postgresql.org/pub/repos/apt bookworm-pgdg main" > /etc/apt/sources.list.d/pgdg.list && \
+    wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+RUN apt-get update && \
+    apt-get install -y postgresql-client-16 && \
+    rm -rf /var/lib/apt/lists/*
+COPY . .
+RUN pip install -r requirements/requirements.txt
+RUN pip install -r requirements/dev-requirements.txt
+EXPOSE 5050
+CMD ["flask", "run", "--debug"]
---
+++ application/config.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    APP_ROOT = os.path.abspath(os.path.dirname(__file__))
+    PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = False
+    DEBUG = False
+    AUTHENTICATION_ON = True
+    GOOGLE_PRIVATE_KEY_ID = os.getenv("GOOGLE_PRIVATE_KEY_ID")
+    GOOGLE_PRIVATE_KEY = os.getenv("GOOGLE_PRIVATE_KEY")
+    GOOGLE_CLIENT_EMAIL = os.getenv("GOOGLE_CLIENT_EMAIL")
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+    SAFE_URLS = set(os.getenv("SAFE_URLS", "").split(","))
+
+
+class DevelopmentConfig(Config):
+    DEBUG = False
+    ENV = "development"
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+
+class TestConfig(Config):
+    ENV = "test"
+    DEBUG = True
+    TESTING = True
+    SERVER_NAME = "localhost"
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:postgres@localhost/test_considerations"
+    )
+    AUTHENTICATION_ON = False
